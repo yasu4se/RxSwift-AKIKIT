bb3d74b46c088e513e17f89b4c0c58aa7f3c5668
yasushi saitoh <yasushi.saitoh@my.latteart-fan.info>
2021-09-25 17:58:12 +0900

    Initial Commit

---
+++ WikiRxSwiftHelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WikiRxSwiftHelloWorld
+//
+//  Created by yasushi saitoh on 2021/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
